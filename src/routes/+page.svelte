f08cc8bc7deea871e2e55bf12e753ba3568fcf2b
Todor Garnev <tgarnev@abv.bg>
2023-08-06 21:58:56 +0300

    tested some daisy ui components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<select class="select select-bordered w-full max-w-xs">
+  <option disabled selected>Who shot first?</option>
+  <option>Han Solo</option>
+  <option>Greedo</option>
+</select>
+
+<hr class="my-10" />
+
+<p>Who is this person?</p>
+
+<div class="w-full max-w-xs">
+	<label class="label cursor-pointer justify-start">
+    <input type="radio" name="radio-10" class="radio mr-4 checked:bg-red-500" />
+    <span class="label-text">Red pill</span>
+  </label>
+</div>
+
+<div class="w-full max-w-xs">
+  <label class="label cursor-pointer">
+    <input type="radio" name="radio-10" class="radio checked:bg-blue-500" />
+    <span class="label-text">Blue pill</span>
+  </label>
+</div>

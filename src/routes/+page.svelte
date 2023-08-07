0ce93b8b099caaceac551a2cb328ee13d6b7b199
Todor Garnev <tgarnev@abv.bg>
2023-08-07 15:01:08 +0300

    added a request to openai api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,19 @@
 <select class="select select-bordered w-full max-w-xs">
-  <option disabled selected>Who shot first?</option>
-  <option>Han Solo</option>
-  <option>Greedo</option>
+	<option disabled selected>Who shot first?</option>
+	<option>Han Solo</option>
+	<option>Greedo</option>
 </select>
 
 <hr class="my-10" />
 
 <p>Who is this person?</p>
 
-<div class="w-full max-w-xs">
-	<label class="label cursor-pointer justify-start">
-    <input type="radio" name="radio-10" class="radio mr-4 checked:bg-red-500" />
-    <span class="label-text">Red pill</span>
-  </label>
-</div>
+<label class="w-fit label cursor-pointer">
+	<input type="radio" name="radio-10" class="radio mr-4 checked:bg-red-500" />
+	<span class="label-text">Red pill</span>
+</label>
 
-<div class="w-full max-w-xs">
-  <label class="label cursor-pointer">
-    <input type="radio" name="radio-10" class="radio checked:bg-blue-500" />
-    <span class="label-text">Blue pill</span>
-  </label>
-</div>
+<label class="w-fit label cursor-pointer">
+	<input type="radio" name="radio-10" class="radio mr-4 checked:bg-blue-500" />
+	<span class="label-text">Blue pill</span>
+</label>
